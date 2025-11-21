425bf75ff2be9ae461d3fdc97f5d9b4d5c7081f2
Nguyễn Thành Trọng <tronglele.mail@gmail.com>
2025-11-21 08:37:16 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Portfolio của Nguyễn Thành Trọng đã load xong!');
+
+    alert('Chào mừng bạn đến với portfolio thật trên Internet của mình!');
+});
